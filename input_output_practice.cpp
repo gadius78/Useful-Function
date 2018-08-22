40189bb1333863a203b6efb5aabaa8f8772cb955
TER\minbyeon <tkfkddlfk77@gmail.com>
2018-08-22 14:36:44 +0900

    It is entered regardless of the data type.

---
+++ input_output_practice.cpp
@@ -0,0 +1,20 @@
+#include<cstdio>
+#include<iostream>
+#include<cstdlib>
+#include<vector>
+
+
+using namespace std;
+int main(){
+	int input=0;
+	int index=0;
+	while(scanf("%i",&input) == 1){
+	
+		
+		printf("%d\n",input);
+		
+	}
+
+
+	return 0;
+} // It is entered regardless of the data type.
