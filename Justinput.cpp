c028eda4c7e6a8dbbeca5be06b5beed5db541662
TER\minbyeon <tkfkddlfk77@gmail.com>
2018-08-22 14:37:10 +0900

    Print as it is entered / Until you get an Enter

---
+++ Justinput.cpp
@@ -0,0 +1,18 @@
+#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<cstdlib>
+
+using namespace std;
+
+int main(){
+	char str[20];
+	while(scanf("%[^\n]",str)==1){
+			printf("%s\n",str);
+	}
+	
+	
+	
+		
+	return 0;
+} // Print as it is entered / Until you get an Enter
