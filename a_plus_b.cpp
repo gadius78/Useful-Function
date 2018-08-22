74118d76d2d2d553e223a8e2b7ef05d4ddf14d88
TER\minbyeon <tkfkddlfk77@gmail.com>
2018-08-22 14:35:14 +0900

    // ADD 2 input / as long as input1 and input2 are existed

---
+++ a_plus_b.cpp
@@ -0,0 +1,17 @@
+#include<cstdio>
+#include<iostream>
+#include<cstdlib>
+#include<vector>
+
+using namespace std;
+int main(){
+	int i=0;
+	int input1=0,input2=0;
+	while(scanf("%d %d",&input1, &input2) == 2){
+		cout << input1 +input2 << endl;
+	
+	}
+
+
+	return 0;
+} // ADD 2 input / as long as input1 and input2 are existed
